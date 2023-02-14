7f6da8d7da7af13784b5f927912bd49ce502c5ea
Wa10 <walace.alves10@gmail.com>
2023-02-14 10:50:24 -0300

    first commit

---
+++ src/main/java/com/teste/cadusuario/CadusuarioApplication.java
@@ -0,0 +1,13 @@
+package com.teste.cadusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadusuarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadusuarioApplication.class, args);
+    }
+
+}
