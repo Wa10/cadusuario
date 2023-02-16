3e3010c2a787e38633015a66358dc2e0ad827de3
Wa10 <walace.alves10@gmail.com>
2023-02-16 13:05:13 -0300

    habilita cors

--- src/main/java/com/teste/cadusuario/resources/FuncionarioResource.java
+++ src/main/java/com/teste/cadusuario/resources/FuncionarioResource.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/funcionarios")
 public class FuncionarioResource {
 
